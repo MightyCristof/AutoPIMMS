afc9dee91b8b5dd28aca5e2b45e4654ae40e8c3d
Rosa Wallace Everson <71036966+rosawe@users.noreply.github.com>
2021-06-22 14:18:23 -0700

    Update outline.py

--- outline.py
+++ outline.py
@@ -11,25 +11,24 @@
 ### data loading and parsing ###
 # allow user to input the name of the data file?
 # load data file with column names into array, list, etc.
-# more efficient to only load varying axis vals? would this limit future functionality?
 
-### WebPIMMS vals input, submission ###
+### "fetch" WebPIMMS vals input, submission, results parse ###
 # using mechanize package Forms API, input values and menu selections based on known HTML tags
 # all values remain fixed at each iteration except varying axis
 # submit using mechanize.HTMLForm.click()
-
-### WebPIMMS results parse, save ###
 # after submission, parse results page for correct line of text
+
+### saving results to temp list ###
 # save the output value to array, list, etc.
 
 ### iterative loop ###
 # this should talk to the data array for how many steps to iterate
+# loads single line of data into object of params class and passes to "fetch"
 # the WebPIMMS functions might be nested inside this function so it makes a call to the webpage
 # for each iteration
-# maybe need to use mechanize.Browser for this?
 
 ### data output ###
-# what data structure makes sense for this? concatenate to input data?
+# concatenate to input data and write out to new text file
 
 #------------------------------------------------------------------------------------------------#
 
@@ -37,3 +36,4 @@
 # - user selection of varying data axis
 # - multiple varying axes
 # - loading of customized input/output ranges based on instrument
+# - adding functionality for differing data structures
