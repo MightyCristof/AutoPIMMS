717b06415c23bf324a04acc3b8298062d8f97186
Rosa W. Everson <rosa@ucsc.edu>
2021-06-22 20:48:28 -0700

    addressed delimiter issue, check output file

--- source/inout.py
+++ source/inout.py
@@ -1,5 +1,6 @@
 # functions to control input and outfile files and data structures
 
+from os import sep
 import pandas as pd
 
 # paths are set to demo, but should be an available user argument 
@@ -10,12 +11,13 @@ infile = 'example_input.txt'
 outfile = 'example_output.txt'
 
 def load_data(path, filename):
-    data = pd.read_csv(path+filename, delimiter="\t")
+    data = pd.read_csv(path+filename, delim_whitespace=True)
     return data
 
 def save_data(path, filename, data):
-    data.to_csv(path+filename, sep="\t", index=False)
+    data.to_csv(path+filename, index=False, sep="\t")
 
-data = load_data(filepath,infile)
+# quick tests
+#data = load_data(filepath,infile)
 
-save_data(filepath,outfile,data)
+#save_data(filepath,outfile,data)
